f7b0227d5180db97352e80f98d5f6397c6f39222
James Wang <wangjamesl18@gmail.com>
2017-01-25 11:19:50 -0800

    Add sounds

--- Synthesizer/app/src/main/java/com/example/wang55459/synthesizer/SynthesizerActivity.java
+++ Synthesizer/app/src/main/java/com/example/wang55459/synthesizer/SynthesizerActivity.java
@@ -1,12 +1,13 @@
 package com.example.wang55459.synthesizer;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class SynthesizerActivity extends AppCompatActivity implements View.OnClickListener{
+public class SynthesizerActivity extends AppCompatActivity{ //implements View.OnClickListener{
     private static final String TAG =
             SynthesizerActivity.class.getName();
     private Button button1;
@@ -18,12 +19,21 @@ public class SynthesizerActivity extends AppCompatActivity implements View.OnCli
         setContentView(R.layout.activity_synthesizer);
         button1 = (Button)findViewById(R.id.button1);
         button2 = (Button)findViewById(R.id.button2);
+        button2 = (Button)findViewById(R.id.button2);
+        mpE = MediaPlayer.create(this, R.raw.scalee);
+        mpF = MediaPlayer.create(this, R.raw.scalef);
+
     }
 
-    @Override
 
-    public void onClick(View view) {
-        Log.i(TAG, "Button Clicked");
+    public void onButtonClick1(View v) {
+        Log.i(TAG, "Button 1 Clicked");
+        mpE.start();
+
+    }
+    public void onButtonClick2(View v) {
+        Log.i(TAG, "Button 2 Clicked");
+        mpE.start();
 
     }
 }
