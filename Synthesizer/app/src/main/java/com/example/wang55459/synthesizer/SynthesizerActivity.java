89563fe4c402ef202d416b9f3531c9b6d6991f55
James Wang <jamesw2320tablet@gmail.com>
2017-01-25 23:00:02 -0800

    update

--- Synthesizer/app/src/main/java/com/example/wang55459/synthesizer/SynthesizerActivity.java
+++ Synthesizer/app/src/main/java/com/example/wang55459/synthesizer/SynthesizerActivity.java
@@ -8,7 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class SynthesizerActivity extends AppCompatActivity{ //implements View.OnClickListener{
+public class SynthesizerActivity extends AppCompatActivity {
     private static final String TAG =
             SynthesizerActivity.class.getName();
     private Button button1;
@@ -16,28 +16,28 @@ public class SynthesizerActivity extends AppCompatActivity{ //implements View.On
     private MediaPlayer mpE;
     private MediaPlayer mpF;
 
+    public void onButtonClick1(View v) {
+        Log.e(TAG, "Button 1 Clicked");
+        mpE.start();
+    }
+
+    public void onButtonClick2(View v) {
+        Log.e(TAG, "Button 2 Clicked");
+        mpF.start();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_synthesizer);
-        button1 = (Button)findViewById(R.id.button1);
-        button2 = (Button)findViewById(R.id.button2);
-        button2 = (Button)findViewById(R.id.button2);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
         mpE = MediaPlayer.create(this, R.raw.scalee);
         mpF = MediaPlayer.create(this, R.raw.scalef);
-
     }
 
 
-    public void onButtonClick1(View v) {
-        Log.i(TAG, "Button 1 Clicked");
-        mpE.start();
-
-    }
-    public void onButtonClick2(View v) {
-        Log.i(TAG, "Button 2 Clicked");
-        mpE.start();
-
-    }
 }
+
+
+
 
