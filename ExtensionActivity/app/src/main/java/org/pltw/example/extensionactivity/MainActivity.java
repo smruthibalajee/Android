61fe023d939ae29b96592a41351ec69f9e77c43f
James Wang <Wang55459>
2017-01-31 10:44:51 -0800

    create first two songs

--- ExtensionActivity/app/src/main/java/org/pltw/example/extensionactivity/MainActivity.java
+++ ExtensionActivity/app/src/main/java/org/pltw/example/extensionactivity/MainActivity.java
@@ -3,10 +3,12 @@ package org.pltw.example.extensionactivity;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
     private final int WHOLE_NOTE = 1000;
     private final int HALF_NOTE = 500;
 
@@ -31,25 +33,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private MediaPlayer mpHighFs;
     private MediaPlayer mpHighG;
 
-    private static void Song1(){
 
-    }
-
-    private static void Song2(){
-
-    }
-
-    private static void Song3(){
-
-    }
-
-    private static void Song4(){
-
-    }
-
-    private static void Song5(){
-
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,7 +65,86 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mpHighG = MediaPlayer.create(this, R.raw.scalehighg);
         mpG = MediaPlayer.create(this, R.raw.scaleg);
         mpGs = MediaPlayer.create(this, R.raw.scalegs);
-     
+
+    }
+
+
+    private void delayPlaying(int delay) throws InterruptedException {
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+            Log.e("Synthesizer", "Audio Playback Interrupted");
+        }
+    }
+
+    private void Song1(){ //Scales a --> g
+        try{
+            mpA.start();
+            delayPlaying(HALF_NOTE);
+            mpA.seekTo(0);
+            mpB.start();
+            delayPlaying(HALF_NOTE);
+            mpB.seekTo(0);
+            mpC.start();
+            delayPlaying(HALF_NOTE);
+            mpC.seekTo(0);
+            mpD.start();
+            delayPlaying(HALF_NOTE);
+            mpD.seekTo(0);
+            mpE.start();
+            delayPlaying(HALF_NOTE);
+            mpE.seekTo(0);
+            mpF.start();
+            delayPlaying(HALF_NOTE);
+            mpF.seekTo(0);
+            mpG.start();
+            delayPlaying(HALF_NOTE);
+            mpG.seekTo(0);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void Song2(){ //Scales g --> a
+        try{
+            mpG.start();
+            delayPlaying(HALF_NOTE);
+            mpG.seekTo(0);
+            mpF.start();
+            delayPlaying(HALF_NOTE);
+            mpF.seekTo(0);
+            mpE.start();
+            delayPlaying(HALF_NOTE);
+            mpE.seekTo(0);
+            mpD.start();
+            delayPlaying(HALF_NOTE);
+            mpD.seekTo(0);
+            mpC.start();
+            delayPlaying(HALF_NOTE);
+            mpC.seekTo(0);
+            mpB.start();
+            delayPlaying(HALF_NOTE);
+            mpB.seekTo(0);
+            mpA.start();
+            delayPlaying(HALF_NOTE);
+            mpA.seekTo(0);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void Song3(){
+
+    }
+
+    private static void Song4(){
+
+    }
+
+    private static void Song5(){
+
     }
 
     @Override
