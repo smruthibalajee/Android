5c1a02dac8b2635806d9a13d9821962aadc2e9e4
James Wang <Wang55459>
2017-02-08 11:21:13 -0800

    k

--- MakeupAssign-Q11/RunBenchmark.java
+++ MakeupAssign-Q11/RunBenchmark.java
@@ -32,27 +32,35 @@
  */
 public class RunBenchmark
 {
- 
-   RandAry rA;
-   int nums[];
-    public RunBenchmark(int testSize)
-    {
-    rA=new RandAry(testSize);
-    nums=rA.getRandAry();
-    
-    //System.out.println("NUMS ARRAY:");
-    //printA();
-    
-    long startTime = System.nanoTime();
-    int result=getLowest(nums);
-    long deltaT=(System.nanoTime()-startTime)/1000;
-    System.out.println("Lowest value: "+result+"\nfor testSize="+testSize+ " completed in "+deltaT+" msec");
-     
-}
 
-   public void printA(){
-       rA.printRandAry();
+    RandAry rA;
+    int nums[];
+    
+    public long RunBenchmark()
+    {
+     for(int i = 1; i < 100000000; i = i * 10){
+         System.out.println(RunBenchmark1(i));
+        }
+         return -1;
     }
+    
+    public long RunBenchmark1(int testSize)
+    {
+        rA=new RandAry(testSize);
+        nums=rA.getRandAry();
+
+        long startTime = System.nanoTime();
+        int result=findSecondLowestHash(nums, 2); //K is an arbitrary value, and based on array, performance may change
+        long deltaT=(System.nanoTime()-startTime)/1000;
+        return deltaT;
+      //  System.out.println("Lowest value: "+result+"\nfor testSize="+testSize+ " completed in "+deltaT+" msec");
+    }
+
+    
+    public void printA(){
+        rA.printRandAry();
+    }
+
     public int getLowest(int a[]){
         int lowest=a[0];
         for (int i=1;i<a.length;i++){
@@ -62,51 +70,70 @@ public class RunBenchmark
         }
         return lowest;
     }
-        
-/**
-   * findSecondLowestSimple - loop through an integer array twice, once to find the lowest value, and second to find the second lowest
-   * 
-   * @param a[] an integer array of random values
-   * @return the value of the second lowest element in the array
- **/   
+
+    /**
+     * findSecondLowestSimple - loop through an integer array twice, once to find the lowest value, and second to find the second lowest
+     * 
+     * @param a[] an integer array of random values
+     * @return the value of the second lowest element in the array
+     **/   
     public int findSecondLowestSimple(int a[]){
-      /*
-       *For these tests, you are provided a random array "a" with possible array values in the range [1,a.length+1].
-       *
-       *Algorithm for findSecondLowestSimple
-       *
-       * 1 set both int variables lowestRating and secondLowestRating to the highest possible value of an element in the array
-       * 2 compare each element in the array to the lowestRating. If the current array value is less than the lowestRating, replace the
-       *   lowestRating with the current array value
-       * 3 repeat step 2 to set secondLowestRating, but only replace if secondLowestRating is greater than the current array value and
-       *   lowestRating is less than current array value
-       * 4 Return the secondLoestRating
-      */  
-     
-}
-/**
-   * findSecondLowestHash - store boolean values in a hash table that represents the array. Values are initally false, and then set to true at the
-   * index 1 below the value of the element from the original array.  For instance if a[5]=3, then the boolean value at hash[2] would be set to true.
-   * 
-   * @param a[] an integer array of random values
-   * @param k, the kth lowest position in the array
-   * @return the value of the kth lowest element in the array
- **/  
-     public  int findSecondLowestHash(int a[], int k){
-     /*
-       *For these tests, you are provided a random array "a" with possible array values in the range [1,a.length+1].
-       *
-       * Algorithm for findSecondLowestHash
-       * 
-       * 1 create a boolean array the size of the random array being searched. Note, boolean array elements are initialized to false
-       * 2 for each element in the search array, a, set the hashtable at element a[i] -1 to true
-       * 3 Step through the hashtable, starting at element 0. For each element set to true, increment a counter variable.  Test the counter
-       *   variable at each step against the parameter k, where k represents the kth lowest element. 
-       * 4 if the counter variable equals k, return the curent index +1 (note that in step 2, each hash is shifted by -1.)
-       * 5 if you get to the end of the array and 
-       */
-      
-       
-    
-}
+
+        int smallest = Integer.MAX_VALUE;
+        int secondSmallest = Integer.MAX_VALUE;
+        for (int i = 0; i < a.length; i++) {
+            if(a[i]==smallest){
+                secondSmallest = smallest;
+            } else if (a[i] < smallest) {
+                secondSmallest = smallest;
+                smallest = a[i];
+            } else if (a[i] < secondSmallest) {
+                secondSmallest = a[i];
+            }
+
+        }
+
+        return secondSmallest;
+
+    }
+
+    /**
+     * findSecondLowestHash - store boolean values in a hash table that represents the array. Values are initally false, and then set to true at the
+     * index 1 below the value of the element from the original array.  For instance if a[5]=3, then the boolean value at hash[2] would be set to true.
+     * 
+     * @param a[] an integer array of random values
+     * @param k, the kth lowest position in the array
+     * @return the value of the kth lowest element in the array
+     **/  
+    public  int findSecondLowestHash(int a[], int k){
+        int counter = 0;
+        boolean[] array = new boolean[a.length -1];
+        for(int i = 0; i < array.length; i++){
+            array[i] = true;
+        }
+
+        for(int i = 0; i < array.length; i++){
+            if(array[i] == true){
+                counter++;
+            }
+            if(counter == k){
+                return i + 1;
+            }
+        }
+
+        return -1;
+        /*
+         *For these tests, you are provided a random array "a" with possible array values in the range [1,a.length+1].
+         *
+         * Algorithm for findSecondLowestHash
+         * 
+         * 1 create a boolean array the size of the random array being searched. Note, boolean array elements are initialized to false
+         * 2 for each element in the search array, a, set the hashtable at element a[i] -1 to true
+         * 3 Step through the hashtable, starting at element 0. For each element set to true, increment a counter variable.  Test the counter
+         *   variable at each step against the parameter k, where k represents the kth lowest element. 
+         * 4 if the counter variable equals k, return the curent index +1 (note that in step 2, each hash is shifted by -1.)
+         * 5 if you get to the end of the array and 
+         */
+
+    }
 }
